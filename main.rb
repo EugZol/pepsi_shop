edf01181aae53f31f115320ef9a2a7e23381dfcd
Eugene Zolotarev <eugzol@gmail.com>
2019-02-18 23:31:45 +0300

    Added main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,16 @@
+require_relative 'lib/item'
+require_relative 'lib/checkout'
+require_relative 'lib/price_calculator'
 
+checkout = Checkout.new(PriceCalculator)
+
+checkout.add(Item.new('CC')) # 1.5
+checkout.add(Item.new('CC')) # 0
+
+checkout.add(Item.new('PC')) # 2 * 0.8
+checkout.add(Item.new('PC')) # 2 * 0.8
+checkout.add(Item.new('PC')) # 2 * 0.8
+
+checkout.add(Item.new('WA')) # 0.85
+
+puts "%.2f" % checkout.total # 7.15
